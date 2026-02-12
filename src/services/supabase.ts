7a1db0985618ac7104bdfaa8e04cb889ddd6634d
Rahul Suvarna <rahulsuvarna18@gmail.com>
2026-02-12 12:18:38 +0100

    added first web component

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://kevhumqcjphhvglflhgb.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtldmh1bXFjanBoaHZnbGZsaGdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY4NjY3MzMsImV4cCI6MjA1MjQ0MjczM30.vsdX1VUHl_71ZS337OIACPgaIfMXCswurfdY6CwwoEM";
+const supabaseUrl = "https://uwroowxlvfwckdawfaki.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3cm9vd3hsdmZ3Y2tkYXdmYWtpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDY0MTUwMSwiZXhwIjoyMDg2MjE3NTAxfQ.IWtFuz8i30jpOzGJgUdZt6vR9Bh0eTyxNrWXYrGq4VI";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
